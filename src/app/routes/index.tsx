ef6ce96efb29f95fd6919d65e37c36b48ab28c52
Claudio Lessa <caldas271@gmail.com>
2024-08-05 22:25:28 -0300

    feat: add dashboard

---
+++ src/app/routes/index.tsx
@@ -0,0 +1,18 @@
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes as Switch,
+} from "react-router-dom";
+import { Dashboard } from "../pages";
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/dashboard" />} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
